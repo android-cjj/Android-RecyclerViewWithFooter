11100225e9bf82e701596b9dbc48d0364183899a
cjj <929178101@qq.com>
2016-02-01 17:03:36 -0800

    add img

---
+++ app/src/main/java/com/cjj/loadmorervdemo/CustomFootItem.java
@@ -0,0 +1,37 @@
+package com.cjj.loadmorervdemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cjj.FootItem;
+import com.cjj.RecyclerViewWithFooter;
+
+/**
+ * Created by cjj on 2016/2/1.
+ */
+public class CustomFootItem extends FootItem {
+
+    @Override
+    public View onCreateView(ViewGroup parent) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.foot_view,parent,false);
+        return v;
+    }
+
+    @Override
+    public void onBindData(View view, int state) {
+        /**
+         * state 有 RecyclerViewWithFooter 定义的 STATE_END, STATE_LOADING, STATE_EMPTY, STATE_NONE
+         */
+
+        if (state == RecyclerViewWithFooter.STATE_LOADING) {
+
+        } else if (state == RecyclerViewWithFooter.STATE_END) {
+
+
+        }
+
+    }
+
+
+}
---
+++ library/src/main/java/com/cjj/MaterialFootItem.java
@@ -0,0 +1,43 @@
+package com.cjj;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by 普益投 on 2016/2/1.
+ */
+public class MaterialFootItem extends FootItem {
+    private CircleProgressBar mCircleProgressBar;
+    private TextView mEndTextView;
+
+    @Override
+    public View onCreateView(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_material, parent, false);
+        mCircleProgressBar = (CircleProgressBar) view.findViewById(R.id.progress2);
+        mCircleProgressBar.setColorSchemeResources(android.R.color.holo_green_light,android.R.color.holo_orange_light,android.R.color.holo_red_light);
+        mEndTextView = (TextView) view.findViewById(R.id.tv_end);
+        return view;
+    }
+
+    @Override
+    public void onBindData(View view, int state) {
+        if (state == RecyclerViewWithFooter.STATE_LOADING) {
+            mCircleProgressBar.setVisibility(View.VISIBLE);
+            mEndTextView.setVisibility(View.GONE);
+        } else if (state == RecyclerViewWithFooter.STATE_END) {
+
+            showEnd(endText);
+        }
+    }
+
+    public void showEnd(CharSequence end) {
+        mEndTextView.setVisibility(View.VISIBLE);
+        mCircleProgressBar.setVisibility(View.GONE);
+        if (!TextUtils.isEmpty(end)) {
+            mEndTextView.setText(end);
+        }
+    }
+}
